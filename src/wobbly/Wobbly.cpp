9453509eb6bc2a5ccda519f38fa91dc5ae25f64d
cantabile <cantabile.desu@gmail.com>
2015-06-26 12:57:10 +0300

    Handle static Windows builds of Qt

--- src/wobbly/Wobbly.cpp
+++ src/wobbly/Wobbly.cpp
@@ -3,6 +3,18 @@
 #include "WobblyWindow.h"
 
 
+#ifdef WOBBLY_STATIC_QT
+
+#ifdef _WIN32
+#include <QtPlugin>
+Q_IMPORT_PLUGIN (QWindowsIntegrationPlugin);
+#else
+#error "Not sure what to do with a static Qt on this platform. File a bug report."
+#endif // _WIN32
+
+#endif // WOBBLY_STATIC_QT
+
+
 int main(int argv, char **args) {
     QApplication app(argv, args);
 
