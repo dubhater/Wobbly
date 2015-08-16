03ea01cf6a55ac38421e4c3956be3a2ce311ef15
cantabile <cantabile.desu@gmail.com>
2015-08-16 15:16:37 +0300

    Add help menus
    Kind of useless.

--- src/wobbly/WobblyWindow.cpp
+++ src/wobbly/WobblyWindow.cpp
@@ -245,6 +245,40 @@ void WobblyWindow::createMenu() {
 
 
     tools_menu = bar->addMenu("&Tools");
+
+
+    QMenu *h = bar->addMenu("&Help");
+
+    QAction *helpAbout = new QAction("About", this);
+    QAction *helpAboutQt = new QAction("About Qt", this);
+
+    connect(helpAbout, &QAction::triggered, [this] () {
+        QMessageBox::about(this, QStringLiteral("About Wobbly"), QStringLiteral(
+            "<a href='https://github.com/dubhater/Wobbly'>https://github.com/dubhater/Wobbly</a><br />"
+            "<br />"
+            "Copyright (c) 2015, John Smith<br />"
+            "<br />"
+            "Permission to use, copy, modify, and/or distribute this software for "
+            "any purpose with or without fee is hereby granted, provided that the "
+            "above copyright notice and this permission notice appear in all copies.<br />"
+            "<br />"
+            "THE SOFTWARE IS PROVIDED \"AS IS\" AND THE AUTHOR DISCLAIMS ALL "
+            "WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED "
+            "WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR "
+            "BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES "
+            "OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, "
+            "WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, "
+            "ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS "
+            "SOFTWARE."
+        ));
+    });
+
+    connect(helpAboutQt, &QAction::triggered, [this] () {
+        QMessageBox::aboutQt(this);
+    });
+
+    h->addAction(helpAbout);
+    h->addAction(helpAboutQt);
 }
 
 
