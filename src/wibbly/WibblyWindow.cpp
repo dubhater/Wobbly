dubhater/Wobbly

commit 6347fed118e92289fe970ae2cf0d887b74677a4a

Make QImage free the VapourSynth frames automatically

src/wibbly/WibblyWindow.cpp
@@ -55,7 +55,6 @@ WibblyWindow::WibblyWindow()
     , vscore(nullptr)
     , vsnode(nullptr)
     , vsvi(nullptr)
-    , vsframe(nullptr)
     , current_frame(0)
     , trim_start(-1)
     , trim_end(-1)
@@ -152,8 +151,6 @@ void WibblyWindow::initialiseVapourSynth() {
 
 void WibblyWindow::cleanUpVapourSynth() {
     video_frame_label->setPixmap(QPixmap());
-    vsapi->freeFrame(vsframe);
-    vsframe = nullptr;
 
     vsapi->freeNode(vsnode);
     vsnode = nullptr;
@@ -1220,8 +1217,6 @@ void WibblyWindow::evaluateFinalScript(int job_index) {
     vsvi = vsapi->getVideoInfo(vsnode);
 
     video_frame_label->setPixmap(QPixmap());
-    vsapi->freeFrame(vsframe);
-    vsframe = nullptr;
 }
 
 
@@ -1328,12 +1323,9 @@ void WibblyWindow::displayFrame(int n) {
     int width = vsapi->getFrameWidth(frame, 0);
     int height = vsapi->getFrameHeight(frame, 0);
     int stride = vsapi->getStride(frame, 0);
-    QPixmap pixmap = QPixmap::fromImage(QImage(ptr, width, height, stride, QImage::Format_RGB32).mirrored(false, true));
+    QPixmap pixmap = QPixmap::fromImage(QImage(ptr, width, height, stride, QImage::Format_RGB32, (QImageCleanupFunction)vsapi->freeFrame, (void *)frame).mirrored(false, true));
 
     video_frame_label->setPixmap(pixmap);
-    // Must free the frame only after replacing the pixmap.
-    vsapi->freeFrame(vsframe);
-    vsframe = frame;
 
     current_frame = n;
 

src/wobbly/WobblyWindow.cpp
@@ -69,7 +69,6 @@ WobblyWindow::WobblyWindow()
     , vsscript(nullptr)
     , vscore(nullptr)
     , vsnode{nullptr, nullptr}
-    , vsframes{ }
 {
     createUI();
 
@@ -2580,11 +2579,8 @@ void WobblyWindow::initialiseVapourSynth() {
 
 void WobblyWindow::cleanUpVapourSynth() {
     frame_label->setPixmap(QPixmap());
-    for (int i = 0; i < NUM_THUMBNAILS; i++) {
+    for (int i = 0; i < NUM_THUMBNAILS; i++)
         thumb_labels[i]->setPixmap(QPixmap());
-        vsapi->freeFrame(vsframes[i]);
-        vsframes[i] = nullptr;
-    }
 
     for (int i = 0; i < 2; i++) {
         vsapi->freeNode(vsnode[i]);
@@ -3772,7 +3768,7 @@ void WobblyWindow::frameDone(void *framev, int n, void *nodev, const QString &er
     int width = vsapi->getFrameWidth(frame, 0);
     int height = vsapi->getFrameHeight(frame, 0);
     int stride = vsapi->getStride(frame, 0);
-    QImage image(ptr, width, height, stride, QImage::Format_RGB32);
+    QImage image(ptr, width, height, stride, QImage::Format_RGB32, (QImageCleanupFunction)vsapi->freeFrame, (void *)frame);
     QPixmap pixmap = QPixmap::fromImage(image.mirrored(false, true));
 
     int offset;
@@ -3789,9 +3785,6 @@ void WobblyWindow::frameDone(void *framev, int n, void *nodev, const QString &er
     pixmap = pixmap.scaled(pixmap.size() / 5, Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
 
     thumb_labels[offset + NUM_THUMBNAILS / 2]->setPixmap(pixmap);
-    // Must free the frame only after replacing the pixmap.
-    vsapi->freeFrame(vsframes[offset + NUM_THUMBNAILS / 2]);
-    vsframes[offset + NUM_THUMBNAILS / 2] = frame;
 
     if (!pending_requests && pending_frame != current_frame)
         requestFrames(current_frame);

src/wibbly/WibblyWindow.h
@@ -106,7 +106,6 @@ class WibblyWindow : public QMainWindow
     VSCore *vscore;
     VSNodeRef *vsnode;
     const VSVideoInfo *vsvi;
-    const VSFrameRef *vsframe;
 
 
     // Other stuff.

src/wobbly/WobblyWindow.h
@@ -207,7 +207,6 @@ private:
     VSScript *vsscript;
     VSCore *vscore;
     VSNodeRef *vsnode[2];
-    const VSFrameRef *vsframes[NUM_THUMBNAILS];
 
 
     // Functions

commit 9aa6d984e9b93739c734c8207e484d8dd3511b7e

Fix crash in 32 bit Windows
Bug introduced in 6347fed118e92289fe970ae2cf0d887b74677a4a.

QImageCleanupFunction needs to be a cdecl function. VSAPI::freeFrame is
a stdcall function. Add a cdecl wrapper function to avoid the crash.

src/wibbly/WibblyWindow.cpp
@@ -48,6 +48,13 @@ std::mutex requests_mutex;
 std::condition_variable requests_condition;
 
 
+// QImageCleanupFunction is expected to be a cdecl function, but VSAPI::freeFrame uses stdcall.
+// Thus a wrapper is needed.
+void vsapiFreeFrameCdecl(void *frame) {
+    vsscript_getVSApi()->freeFrame((const VSFrameRef *)frame);
+}
+
+
 WibblyWindow::WibblyWindow()
     : QMainWindow()
     , vsapi(nullptr)
@@ -1323,7 +1330,7 @@ void WibblyWindow::displayFrame(int n) {
     int width = vsapi->getFrameWidth(frame, 0);
     int height = vsapi->getFrameHeight(frame, 0);
     int stride = vsapi->getStride(frame, 0);
-    QPixmap pixmap = QPixmap::fromImage(QImage(ptr, width, height, stride, QImage::Format_RGB32, (QImageCleanupFunction)vsapi->freeFrame, (void *)frame).mirrored(false, true));
+    QPixmap pixmap = QPixmap::fromImage(QImage(ptr, width, height, stride, QImage::Format_RGB32, vsapiFreeFrameCdecl, (void *)frame).mirrored(false, true));
 
     video_frame_label->setPixmap(pixmap);
 

src/wobbly/WobblyWindow.cpp
@@ -50,6 +50,13 @@ SOFTWARE.
 #include "WobblyWindow.h"
 
 
+// QImageCleanupFunction is expected to be a cdecl function, but VSAPI::freeFrame uses stdcall.
+// Thus a wrapper is needed.
+void vsapiFreeFrameCdecl(void *frame) {
+    vsscript_getVSApi()->freeFrame((const VSFrameRef *)frame);
+}
+
+
 WobblyWindow::WobblyWindow()
     : QMainWindow()
     , import_window(nullptr)
@@ -3927,7 +3934,7 @@ void WobblyWindow::frameDone(void *framev, int n, void *nodev, const QString &er
     int width = vsapi->getFrameWidth(frame, 0);
     int height = vsapi->getFrameHeight(frame, 0);
     int stride = vsapi->getStride(frame, 0);
-    QImage image = QImage(ptr, width, height, stride, QImage::Format_RGB32, (QImageCleanupFunction)vsapi->freeFrame, (void *)frame).mirrored(false, true);
+    QImage image = QImage(ptr, width, height, stride, QImage::Format_RGB32, vsapiFreeFrameCdecl, (void *)frame).mirrored(false, true);
 
     int offset;
     if (node == vsnode[0])